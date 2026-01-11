34e6ad9957a22286b8abdcbb9fe788870731182a
ProClifo <proclifo@gmail.com>
2026-01-11 21:53:30 +0100

    Update minui.c

--- workspace/all/minui/minui.c
+++ workspace/all/minui/minui.c
@@ -1649,7 +1649,7 @@ int main (int argc, char *argv[]) {
 			
 				// buttons
 				if (show_setting && !GetHDMI()) GFX_blitHardwareHints(screen, show_setting);
-				else if (can_resume) GFX_blitButtonGroup((char*[]){ "X","RESUME",  NULL }, 0, screen, 0);
+				else if (can_resume) GFX_blitButtonGroup((char*[]){ "A","RESUME",  NULL }, 0, screen, 0);
 				else GFX_blitButtonGroup((char*[]){ 
 					BTN_SLEEP==BTN_POWER?"POWER":"MENU",
 					BTN_SLEEP==BTN_POWER||simple_mode?"SLEEP":"INFO",  
